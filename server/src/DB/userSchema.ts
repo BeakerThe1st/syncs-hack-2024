c311411c2c2e003af533144b983e3500a41ce641
roscida1 <joshuaphillips910@gmail.com>
2024-08-24 18:43:47 +1000

    fixed songid being a number rather than string

--- server/src/DB/userSchema.ts
+++ server/src/DB/userSchema.ts
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 interface IUser {
     UserID: number;
-    SongID: number;
+    SongID: string;
 }
 
 const UserSchema = new Schema<IUser>({
     UserID: { type: Number, required: true },
-    SongID: { type: Number, required: true },
+    SongID: { type: String, required: true },
 })
 
 const User = mongoose.model<IUser>("User", UserSchema)
