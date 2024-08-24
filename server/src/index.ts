0834f71069783257ecdcb6064a6c697e6a9f177e
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-08-24 13:56:21 +1000

    Cleanup token route

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ var client_secret = process.env.CLIENT_SECRET;
 var redirect_uri = 'http://localhost:6969/callback';
 
 import Express from 'express';
+
 const app = Express();
 
 app.get("/", (req, res) => {
@@ -13,32 +14,30 @@ app.listen(6969, () => {
     console.log(`Listening on 6969`);
 })
 
-function getServerToken(): string {
-  const token = await fetch('', {
-    method: 'POST',
-    headers: "Content-Type: application/x-www-form-urlencoded",
-    body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
-  });
-  return token; 
-}
+/*const getServerToken = async (): string => {
+    return await fetch('', {
+        method: 'POST',
+        headers: "Content-Type: application/x-www-form-urlencoded",
+        body: `grant_type=client_credentials&client_id=${client_id}&client_secret=${client_secret}`
+    });
+}*/
 
-app.get("/token/", (req, res) => {
-  const code = req.params.code;
-  const accessToken = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
+app.get("/token/", async (req, res) => {
+  const code = ((req.params as any).code as string);
+  const response = await fetch("http://accounts.spotify.com/api/token", {
+      body: JSON.stringify({
+          code,
+          redirect_uri,
+          grant_type: 'authorization_code'
+      }),
       headers: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + (Buffer.from(client_id + ':' + client_secret).toString('base64'))
+      }
+  });
+  const json = await response.json();
 
-  return accessToken;
+  res.status(200).send({accessToken: json.accessToken});
 })
 
 console.log("hello world");
