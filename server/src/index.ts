29eada4417c1f7f97d07f10126a7f3846c097d44
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-08-24 12:23:59 +1000

    Commit base web and backend

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import Express from 'express';
+const app = Express();
+
+app.get("/", (req, res) => {
+    return res.status(200).send("hello poopy");
+});
+
+app.listen(6969, () => {
+    console.log(`Listening on 6969`);
+})
+
+console.log("hello world");
