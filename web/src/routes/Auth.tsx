504cca575fc7fe126d709fce483e85d6edf796c2
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-08-24 13:35:00 +1000

    Update auth component to hopefully call api

--- web/src/routes/Auth.tsx
+++ web/src/routes/Auth.tsx
@@ -1,11 +1,20 @@
-import {useSearchParams} from "react-router-dom";
+import {Navigate, useNavigate, useSearchParams} from "react-router-dom";
 import {useEffect} from "react";
 
 export const Auth = () => {
-    const [searchParams, setSearchParams] = useSearchParams();
+    const [searchParams] = useSearchParams();
+    const code = searchParams.get("code");
+    const navigate = useNavigate();
+    if (!code) {
+        return <Navigate to={"/"} replace />
+    }
     useEffect(() => {
-        const code = searchParams.get("code");
+        const params = new URLSearchParams({code});
+        fetch(`http://localhost:6969/${params.toString()}`).then((res) => res.json()).then((json) => {
+            localStorage.setItem("accessToken", json.accessToken)
+            navigate({pathname: "/"});
+        })
     }, []);
-    return <h1>Loading... {searchParams.get("code")}</h1>
+    return <h1>Loading auth...</h1>
 }
 
