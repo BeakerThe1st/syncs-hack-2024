902f23a90a831c9d2aec2dd48283d63b378494f9
Adam <31784045+BeakerThe1st@users.noreply.github.com>
2024-08-24 18:04:55 +1000

    Fix player

--- web/src/routes/Player.tsx
+++ web/src/routes/Player.tsx
@@ -1,19 +1,13 @@
-import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {useParams} from "react-router-dom";
 
 export const Player = () => {
-    /*const [matchData, setMatchData] = useState<any>();
+    const [matchData, setMatchData] = useState<any>();
     const {match} = useParams();
     useEffect(() => {
-        setMatchData({
-            song_id: "19RybK6XDbAVpcdxSbZL1o"
-        })
         fetch(`http://localhost:6969/match/${match}`).then((res) => res.json()).then((json) => setMatchData(json));
-    }, []);*/
+    }, []);
 
-    const matchData = {
-        song_id: "19RybK6XDbAVpcdxSbZL1o";
-    }
     if (matchData === undefined) {
         return <h1>Loading match data...</h1>
     }
@@ -24,7 +18,8 @@ interface ActualPlayerProps {
     matchData: any
 }
 const ActualPlayer = (props: ActualPlayerProps) => {
-    const [player, setPlayer] = useState<any>(undefined);
+    const [player, setPlayer] = useState<Spotify.Player | undefined>(undefined);
+    const [deviceId, setDeviceId] = useState<string | undefined>();
     const [playerReady, setPlayerReady] = useState(false);
     useEffect(() => {
 
@@ -38,15 +33,36 @@ const ActualPlayer = (props: ActualPlayerProps) => {
             const accessToken = localStorage.getItem("access_token") ?? "";
             console.log(`access token is ${accessToken}`);
             const player = new window.Spotify.Player({
-                name: 'playback',
+                name: 'SpotChat',
                 getOAuthToken: cb => { cb(accessToken); },
                 volume: 0.5
             });
 
-            setPlayer(player);
+            player.on("initialization_error", () => {
+                console.log("init error")
+            })
+            player.on("account_error", () => {
+                console.log("acc error")
+            })
+
+            player.on("authentication_error", () => {
+                console.log("auth error")
+            })
+
+            player.on("playback_error", (err) => {
+                console.dir(err);
+            })
+
+            player.on("autoplay_failed", () => {
+                console.log("autoplay failed");
+            })
+
+            player.on("player_state_changed", () => {
+                console.log("player state changed");
+            })
 
             player.addListener('ready', ({ device_id }) => {
-                console.log('Ready with Device ID', device_id);
+                setDeviceId(device_id);
                 setPlayerReady(true);
             });
 
@@ -54,18 +70,30 @@ const ActualPlayer = (props: ActualPlayerProps) => {
                 console.log('Device ID has gone offline', device_id);
             });
 
+            setPlayer(player);
 
-            player.connect();
+
+            player.connect().then(() => console.log("connected"));
 
         };
-    }, [props]);
+    }, []);
 
-    if (!playerReady) {
+    if (!playerReady || !player || !deviceId) {
         return <h1>Loading Player...</h1>
     }
 
     return <>
-        <button onClick={() => player.togglePlay()}>play/pause</button>
+        <button onClick={() => {
+            fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
+                method: "PUT",
+                body: JSON.stringify({uris: [`spotify:track:${props.matchData.song_id}`]}),
+                headers: {
+                    "Authorization": `Bearer ${localStorage.getItem("access_token")}`
+                }
+            }).catch((e) => {
+                console.dir(e);
+            })
+        }}>Play</button>
         <h1>playing??</h1>
     </>
 }
